eac8d03fb2e456eda9eadbc94bd39a8d796fdcb7
Fedor Indutny <fedor@indutny.com>
2020-11-18 09:05:22 -0800

    fixture: declare variable as extern explicitly
    See: https://github.com/nodejs/llhttp/issues/69#issuecomment-729591610

--- src/native/fixture.h
+++ src/native/fixture.h
@@ -5,7 +5,7 @@
 # define LLPARSE__ERROR_PAUSE 0x7fa73caa
 #endif  /* LLPARSE__ERROR_PAUSE */
 
-int llparse__in_bench;
+extern int llparse__in_bench;
 
 void llparse__print(const char* p, const char* endp, const char* fmt, ...);
 int llparse__print_span(const char* name, const char* p, const char* endp);
